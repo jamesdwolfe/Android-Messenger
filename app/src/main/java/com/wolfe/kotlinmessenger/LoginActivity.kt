37bbb0e1efb49ebab15abafb98113cdcdc58b332
jamesdwolfe <james.wolfe.jr@gmail.com>
2021-03-28 22:54:32 -0700

    Firebase Registration

--- app/src/main/java/com/wolfe/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/wolfe/kotlinmessenger/LoginActivity.kt
@@ -1,14 +1,33 @@
 package com.wolfe.kotlinmessenger
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_register.*
+
+private val TAG = LoginActivity::class.qualifiedName
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        submitLogin.setOnClickListener {
+            Log.d(TAG, "Username is: ${usernameLogin.text.toString()}")
+            Log.d(TAG, "Password is: ${passwordLogin.text.toString()}")
+        }
+
+        registerLogin.setOnClickListener {
+            Log.d(TAG, "Launch register activity")
+//            val intent = Intent(this, RegisterActivity::class.java)
+//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//            startActivity(intent)
+            finish()
+        }
     }
 
 }
--- app/src/main/java/com/wolfe/kotlinmessenger/RegisterActivity.kt
+++ app/src/main/java/com/wolfe/kotlinmessenger/RegisterActivity.kt
@@ -4,7 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
+
+private val TAG = RegisterActivity::class.qualifiedName
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,13 +17,39 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         submitRegister.setOnClickListener {
-            Log.d("RegisterActivity", "Username is: ${usernameRegister.text.toString()}")
-            Log.d("RegisterActivity", "Email is: ${emailRegister.text.toString()}")
-            Log.d("RegisterActivity", "Password is: ${passwordRegister.text.toString()}")
+            val username = usernameRegister.text.toString()
+            val email = emailRegister.text.toString()
+            val password = passwordRegister.text.toString()
+
+            if(email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this,
+                    "Registration failed: Please enter an email and password",
+                    Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            Log.d(TAG, "Username is: $username")
+            Log.d(TAG, "Email is: $email")
+            Log.d(TAG, "Password is: $password")
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) return@addOnCompleteListener
+                    Toast.makeText(this,
+                        "Registration success",
+                        Toast.LENGTH_LONG).show()
+                    Log.d(TAG,"Registration success: UID=${it.result!!.user.uid}")
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this,
+                        "Registration failed: ${it.message}",
+                        Toast.LENGTH_LONG).show()
+                    Log.d(TAG,"Registration failed: ${it.message}")
+                }
         }
 
         loginRegister.setOnClickListener {
-            Log.d("RegisterActivity", "Launch log activity")
+            Log.d(TAG, "Launch login activity")
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
